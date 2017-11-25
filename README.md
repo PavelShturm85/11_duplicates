500cded3fb5885a63ff5fc0d59c1deeb69dcac45
PavelShturm85 <pavel.shturm@gmail.com>
2017-11-25 21:17:58 +0400

    Скрипт ищущий дублированные файлы в директории.

--- README.md
+++ README.md
@@ -1,7 +1,21 @@
 # Anti-Duplicator
 
-[TODO. There will be project description]
+**The module outputs the duplicates of files to the terminal.**
 
+# Quickstart
+**Ways to use:**
+- Have to use  module `duplicates.py` after `python3` with `folder` in terminal.
+
+Example of script launch on Linux, Python 3.5:
+
+```bash
+$ python3 duplicates.py
+Enter the directory: /home/pavel/test
+Duplicate files: /home/pavel/test/3, /home/pavel/test/test2/3, /home/pavel/test/test2/test3/3
+Duplicate files: /home/pavel/test/2, /home/pavel/test/test2/2
+Duplicate files: /home/pavel/test/1, /home/pavel/test/test2/test3/1
+
+```
 # Project Goals
 
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- duplicates.py
+++ duplicates.py
@@ -1,3 +1,38 @@
+import os
+import hashlib
+import sys
+
+
+def find_duplicates(folder):
+    duplicates = {}
+    for dirs, subdirs, files in os.walk(folder):
+        for name in files:
+            full_path_file = os.path.join(dirs, name)
+            file_hash = hashlib.md5(open(full_path_file, 'rb').read()).digest()
+            duplicat = duplicates.get(file_hash)
+            if duplicat:
+                try:
+                    duplicates[file_hash][name].append(full_path_file)
+                except KeyError:
+                    duplicates[file_hash][name] = [full_path_file]
+            else:
+                duplicates[file_hash] = {name: [full_path_file]}
+    return duplicates
+
+
+def print_dublicates(dublicates):
+    for file_hash in duplicates:
+        for full_path_file in duplicates[file_hash]:
+            if len(duplicates[file_hash][full_path_file]) > 1:
+                print('Duplicate files: {}'.format(
+                    ', '.join(duplicates[file_hash][full_path_file])))
+
 
 if __name__ == '__main__':
-    pass
+    if len(sys.argv) > 1:
+        input_file_name = sys.argv[1]
+    else:
+        input_file_name = input('Enter the directory: ')
+
+    duplicates = find_duplicates(input_file_name)
+    print_dublicates(duplicates)
